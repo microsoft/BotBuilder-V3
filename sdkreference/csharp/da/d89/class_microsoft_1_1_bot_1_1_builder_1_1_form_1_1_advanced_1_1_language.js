cd25c3683cf94228ba8910cc4fe513d02794d91a
andreo@microsoft.com <andreo@microsoft.com>
2016-03-09 14:32:59 -0800

    new c# reference files

---
+++ sdkreference/csharp/da/d89/class_microsoft_1_1_bot_1_1_builder_1_1_form_1_1_advanced_1_1_language.js
@@ -0,0 +1,11 @@
+var class_microsoft_1_1_bot_1_1_builder_1_1_form_1_1_advanced_1_1_language =
+[
+    [ "ANormalization", "da/d89/class_microsoft_1_1_bot_1_1_builder_1_1_form_1_1_advanced_1_1_language.html#a02855cc656884545331eec21e9f8c6c7", null ],
+    [ "CamelCase", "da/d89/class_microsoft_1_1_bot_1_1_builder_1_1_form_1_1_advanced_1_1_language.html#aa6d985c2130014da5790b850e8ecbdf0", null ],
+    [ "GenerateTerms", "da/d89/class_microsoft_1_1_bot_1_1_builder_1_1_form_1_1_advanced_1_1_language.html#a560b4b257b0a528bd8cd43187a92dd88", null ],
+    [ "Ignorable", "da/d89/class_microsoft_1_1_bot_1_1_builder_1_1_form_1_1_advanced_1_1_language.html#a70833d2999b8ef050ab94ccbfa0accf9", null ],
+    [ "Ignorable", "da/d89/class_microsoft_1_1_bot_1_1_builder_1_1_form_1_1_advanced_1_1_language.html#a4d74b79cd5aa805cd317da93aafb810e", null ],
+    [ "OptionalPlurals", "da/d89/class_microsoft_1_1_bot_1_1_builder_1_1_form_1_1_advanced_1_1_language.html#a0aa7c632f28bd5f547179c30caa7a6c7", null ],
+    [ "NounStopWords", "da/d89/class_microsoft_1_1_bot_1_1_builder_1_1_form_1_1_advanced_1_1_language.html#acc24c550f306b92d50f8278ada3e4caa", null ],
+    [ "StopWords", "da/d89/class_microsoft_1_1_bot_1_1_builder_1_1_form_1_1_advanced_1_1_language.html#af819bd45d0aa7ff5446616e0ff1d50cd", null ]
+];
