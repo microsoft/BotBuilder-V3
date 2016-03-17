39879f8b1350c4902ab4cfc0f7150875f09ee4c7
Steven Ickman <stevenic@microsoft.com>
2016-03-16 22:13:14 -0700

    Added basic-waterfall example

---
+++ Node/examples/basics-waterfall/app.js
@@ -0,0 +1,37 @@
+/*-----------------------------------------------------------------------------
+This Bot demonstrates how to use a waterfall to prompt the user with a series
+of questions.
+
+This example also shows the user of session.userData to persist information
+about a specific user. Depending on the type of bot you created this could 
+get automatically persisted for you (BotConnectorBot) or you might have to 
+configure your bot to use one of the storage plugins. 
+
+Run the bot from the command line using "node app.js" and then type "hello"
+to wake the bot up.
+-----------------------------------------------------------------------------*/
+
+var builder = require('../../');
+
+var bot = new builder.TextBot();
+bot.add('/', [
+    function (session) {
+        builder.Prompts.text(session, "Hello... What's your name?");
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
+    },
+    function (session, results) {
+        session.userData.coding = results.response;
+        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+    },
+    function (session, results) {
+        session.userData.language = results.response.entity;
+        session.send("Got it... " + session.userData.name + 
+                     " you've been programming for " + session.userData.coding + 
+                     " years and use " + session.userData.language + ".");
+    }
+]);
+
+bot.listenStdin();
