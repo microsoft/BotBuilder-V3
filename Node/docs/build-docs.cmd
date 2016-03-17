d9090fd7f35245ac80c016d723d4fe8e792f1bba
andreo@microsoft.com <andreo@microsoft.com>
2016-03-17 14:22:13 -0700

    renamed docs build file, fixed spacing issue in header of docs template, removed botdirectory search from toolbar

--- Node/docs/build-docs.cmd
+++ Node/docs/build-docs.cmd
@@ -1,13 +1,3 @@
 @ECHO OFF
-IF /i [%1] == [docs] GOTO :docs
-IF /i [%1] == [npm] GOTO :npm
-ECHO usage:
-ECHO To compile docs type "build-library.cmd docs"
-GOTO :end
-
-:docs
 ECHO [COMPILING DOCS]
 typedoc --includeDeclarations --module amd --out doc ..\src\botbuilder.d.ts --theme .\botframework --hideGenerator --name "BotBuilder SDK Reference Library" --readme none
-GOTO :end
-
-:end
