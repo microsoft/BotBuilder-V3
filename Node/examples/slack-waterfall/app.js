757131d0e5d37a7d821f119c41cf53ba0ded48c0
Steven Ickman <stevenic@microsoft.com>
2016-03-17 17:33:31 -0700

    removed slack token from samples

--- Node/examples/slack-waterfall/app.js
+++ Node/examples/slack-waterfall/app.js
@@ -35,7 +35,7 @@ var builder = require('../../');
 
 var controller = Botkit.slackbot();
 var bot = controller.spawn({
-   token: process.env.token || 'xoxb-27353876818-1JaNnDS8yhLSKuzmX9DncWAI'
+   token: process.env.token
 });
 
 var slackBot = new builder.SlackBot(controller, bot);
