2512af149f21bb27656c326bb7b08eb384a550e0
Will Portnoy <wportnoy@microsoft.com>
2016-03-24 11:53:25 -0700

    move IDialogContext to its own file

--- CSharp/Library/Dialogs/IDialog.cs
+++ CSharp/Library/Dialogs/IDialog.cs
@@ -13,41 +13,4 @@ namespace Microsoft.Bot.Builder
     {
         Task StartAsync(IDialogContext context, IAwaitable<T> arguments);
     }
-
-    public delegate Task ResumeAfter<in T>(IDialogContext context, IAwaitable<T> result);
-
-    public interface IDialogStack
-    {
-        void Wait(ResumeAfter<Message> resume);
-        void Call<C, T, R>(C child, T arguments, ResumeAfter<R> resume) where C : class, IDialog<T>;
-        void Done<R>(R value);
-    }
-
-    public interface IBotToUser
-    {
-        Task PostAsync(Message message, CancellationToken cancellationToken = default(CancellationToken));
-    }
-
-    public interface IUserToBot
-    {
-        Task<Message> PostAsync(Message message, CancellationToken cancellationToken = default(CancellationToken));
-    }
-
-    public interface IDialogContext : IBotData, IDialogStack, IBotToUser
-    {
-        Task PostAsync(string text, CancellationToken cancellationToken = default(CancellationToken));
-    }
-
-    public static partial class Extensions
-    {
-        public static void Call<C, T, R>(this IDialogContext context, C child, ResumeAfter<R> resume) where C : class, IDialog<T>
-        {
-            context.Call<C, T, R>(child, default(T), resume);
-        }
-
-        public static void Call<C, T, R>(this IDialogContext context, ResumeAfter<R> resume) where C : class, IDialog<T>, new()
-        {
-            context.Call<C, T, R>(new C(), default(T), resume);
-        }
-    }
 }
---
+++ CSharp/Library/Dialogs/IDialogContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder
+{
+    public delegate Task ResumeAfter<in T>(IDialogContext context, IAwaitable<T> result);
+
+    public interface IDialogStack
+    {
+        void Wait(ResumeAfter<Message> resume);
+        void Call<C, T, R>(C child, T arguments, ResumeAfter<R> resume) where C : class, IDialog<T>;
+        void Done<R>(R value);
+    }
+
+    public interface IBotToUser
+    {
+        Task PostAsync(Message message, CancellationToken cancellationToken = default(CancellationToken));
+    }
+
+    public interface IUserToBot
+    {
+        Task<Message> PostAsync(Message message, CancellationToken cancellationToken = default(CancellationToken));
+    }
+
+    public interface IDialogContext : IBotData, IDialogStack, IBotToUser
+    {
+        Task PostAsync(string text, CancellationToken cancellationToken = default(CancellationToken));
+    }
+
+    public static partial class Extensions
+    {
+        public static void Call<C, T, R>(this IDialogContext context, C child, ResumeAfter<R> resume) where C : class, IDialog<T>
+        {
+            context.Call<C, T, R>(child, default(T), resume);
+        }
+
+        public static void Call<C, T, R>(this IDialogContext context, ResumeAfter<R> resume) where C : class, IDialog<T>, new()
+        {
+            context.Call<C, T, R>(new C(), default(T), resume);
+        }
+    }
+}
