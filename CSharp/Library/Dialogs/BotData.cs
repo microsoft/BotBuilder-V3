d1c0425c9f482ec8ed9ed6c9bae7a54758ee8844
Will Portnoy <wportnoy@microsoft.com>
2016-03-24 11:41:41 -0700

    move IBotData interfaces to separate file

--- CSharp/Library/Dialogs/BotData.cs
+++ CSharp/Library/Dialogs/BotData.cs
@@ -11,36 +11,6 @@ using Microsoft.Bot.Builder.Fibers;
 
 namespace Microsoft.Bot.Builder
 {
-    public interface IBotDataBag
-    {
-        int Count { get; }
-        bool TryGetValue<T>(string key, out T value);
-        void SetValue<T>(string key, T value);
-    }
-
-    public interface IBotData
-    {
-        IBotDataBag UserData { get; }
-
-        IBotDataBag ConversationData { get; }
-
-        IBotDataBag PerUserInConversationData { get; }
-    }
-
-    public static partial class Extensions
-    {
-        public static T Get<T>(this IBotDataBag bag, string key)
-        {
-            T value;
-            if (! bag.TryGetValue(key, out value))
-            {
-                throw new KeyNotFoundException(key);
-            }
-
-            return value;
-        }
-    }
-
     public abstract class BotDataBase<T> : IBotData, Serialization.ISerializeAsReference
     {
         protected readonly Message mesage;
---
+++ CSharp/Library/Dialogs/IBotData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder
+{
+    public interface IBotDataBag
+    {
+        int Count { get; }
+        bool TryGetValue<T>(string key, out T value);
+        void SetValue<T>(string key, T value);
+    }
+
+    public interface IBotData
+    {
+        IBotDataBag UserData { get; }
+
+        IBotDataBag ConversationData { get; }
+
+        IBotDataBag PerUserInConversationData { get; }
+    }
+
+    public static partial class Extensions
+    {
+        public static T Get<T>(this IBotDataBag bag, string key)
+        {
+            T value;
+            if (!bag.TryGetValue(key, out value))
+            {
+                throw new KeyNotFoundException(key);
+            }
+
+            return value;
+        }
+    }
+}
