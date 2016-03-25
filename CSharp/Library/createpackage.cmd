c5c0c3a5df65adb1c4419a9942352668ccb0c2cb
Chris McConnell <chrimc@microsoft.com>
2016-03-25 15:56:42 -0700

    Update nuget packages to use release and semantic versioning.

--- CSharp/Library/createpackage.cmd
+++ CSharp/Library/createpackage.cmd
@@ -1,2 +1,2 @@
 erase /s *.nupkg
-..\packages\NuGet.CommandLine.3.3.0\tools\NuGet.exe pack Microsoft.Bot.Builder.csproj -symbols
+..\packages\NuGet.CommandLine.3.3.0\tools\NuGet.exe pack Microsoft.Bot.Builder.csproj -symbols -build -properties Configuration=release
