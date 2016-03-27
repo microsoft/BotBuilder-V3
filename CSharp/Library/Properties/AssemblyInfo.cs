8361d40397fbc65e36e0163b47098391b514eea1
Chris McConnell <chrimc@microsoft.com>
2016-03-26 17:56:43 -0700

    Update version for package publish.

--- CSharp/Library/Properties/AssemblyInfo.cs
+++ CSharp/Library/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.0.23")]
-[assembly: AssemblyFileVersion("0.9.0.23")]
+[assembly: AssemblyVersion("0.9.0.24")]
+[assembly: AssemblyFileVersion("0.9.0.24")]
 
 [assembly: InternalsVisibleTo("Microsoft.Bot.Builder.Tests")]
 
