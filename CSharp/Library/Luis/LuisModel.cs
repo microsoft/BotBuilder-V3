b80fea00351db92fd93dd314483fad934b89ddc7
Will Portnoy <wportnoy@microsoft.com>
2016-03-28 16:27:24 -0700

    split up LuisService file some more

---
+++ CSharp/Library/Luis/LuisModel.cs
@@ -0,0 +1,71 @@
+﻿// 
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// 
+// Microsoft Bot Framework: http://botframework.com
+// 
+// Bot Builder SDK Github:
+// https://github.com/Microsoft/BotBuilder
+// 
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+// 
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Fibers;
+
+namespace Microsoft.Bot.Builder.Luis
+{
+    /// <summary>
+    /// The LUIS model information.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    [Serializable]
+    public class LuisModel : Attribute
+    {
+        /// <summary>
+        /// The LUIS model ID.
+        /// </summary>
+        public readonly string ModelID;
+
+        /// <summary>
+        /// The LUIS subscription key.
+        /// </summary>
+        public readonly string SubscriptionKey;
+
+        /// <summary>
+        /// Construct the LUIS model information.
+        /// </summary>
+        /// <param name="modelID">The LUIS model ID.</param>
+        /// <param name="subscriptionKey">The LUIS subscription key.</param>
+        public LuisModel(string modelID, string subscriptionKey)
+        {
+            SetField.NotNull(out this.ModelID, nameof(modelID), modelID);
+            SetField.NotNull(out this.SubscriptionKey, nameof(subscriptionKey), subscriptionKey);
+        }
+    }
+}
--- CSharp/Library/Luis/LuisService.cs
+++ CSharp/Library/Luis/LuisService.cs
@@ -1,4 +1,37 @@
-﻿using System;
+﻿// 
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// 
+// Microsoft Bot Framework: http://botframework.com
+// 
+// Bot Builder SDK Github:
+// https://github.com/Microsoft/BotBuilder
+// 
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+// 
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -9,22 +42,6 @@ using Microsoft.Bot.Builder.Fibers;
 
 namespace Microsoft.Bot.Builder.Luis
 {
-    /// <summary>
-    /// The result of a LUIS query.
-    /// </summary>
-    public class LuisResult
-    {
-        /// <summary>
-        /// The intents found in the query text.
-        /// </summary>
-        public IntentRecommendation[] Intents { get; set; }
-
-        /// <summary>
-        /// The entities found in the query text.
-        /// </summary>
-        public EntityRecommendation[] Entities { get; set; }
-    }
-
     /// <summary>
     /// A mockable interface for the LUIS service.
     /// </summary>
@@ -43,35 +60,6 @@ namespace Microsoft.Bot.Builder.Luis
         /// <param name="uri">The query uri.</param>
         /// <returns>The LUIS result.</returns>
         Task<LuisResult> QueryAsync(Uri uri); 
-    }
-
-    /// <summary>
-    /// The LUIS model information.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
-    [Serializable]
-    public class LuisModel : Attribute
-    {
-        /// <summary>
-        /// The LUIS model ID.
-        /// </summary>
-        public readonly string ModelID;
-
-        /// <summary>
-        /// The LUIS subscription key.
-        /// </summary>
-        public readonly string SubscriptionKey;
-
-        /// <summary>
-        /// Construct the LUIS model information.
-        /// </summary>
-        /// <param name="modelID">The LUIS model ID.</param>
-        /// <param name="subscriptionKey">The LUIS subscription key.</param>
-        public LuisModel(string modelID, string subscriptionKey)
-        {
-            SetField.NotNull(out this.ModelID, nameof(modelID), modelID);
-            SetField.NotNull(out this.SubscriptionKey, nameof(subscriptionKey), subscriptionKey);
-        }
     }
 
     /// <summary>
@@ -135,19 +123,6 @@ namespace Microsoft.Bot.Builder.Luis
         {
             var uri = service.BuildUri(text);
             return await service.QueryAsync(uri);
-        }
-
-        /// <summary>
-        /// Try to find an entity within the result.
-        /// </summary>
-        /// <param name="result">The LUIS result.</param>
-        /// <param name="type">The entity type.</param>
-        /// <param name="entity">The found entity.</param>
-        /// <returns>True if the entity was found, false otherwise.</returns>
-        public static bool TryFindEntity(this LuisResult result, string type, out EntityRecommendation entity)
-        {
-            entity = result.Entities?.FirstOrDefault(e => e.Type == type);
-            return entity != null;
         }
     }
 }
