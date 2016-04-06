ba25b18238a1ceda52924b2b435906cc162b57f3
Will Portnoy <wportnoy@microsoft.com>
2016-04-06 11:11:11 -0700

    luis: add intent handler delegate validation

--- CSharp/Tests/Microsoft.Bot.Builder.Tests/LuisTests.cs
+++ CSharp/Tests/Microsoft.Bot.Builder.Tests/LuisTests.cs
@@ -44,71 +44,91 @@ using Microsoft.Bot.Builder.Dialogs;
 
 namespace Microsoft.Bot.Builder.Tests
 {
-    public sealed class DerivedLuisDialog : LuisDialog<object>
-    {
-        public DerivedLuisDialog(ILuisService service)
-            : base(service)
-        {
-        }
-
-        [LuisIntent("PublicHandlerWithAttribute")]
-        public Task PublicHandlerWithAttribute(IDialogContext context, LuisResult luisResult)
-        {
-            throw new NotImplementedException();
-        }
-
-        [LuisIntent("PrivateHandlerWithAttribute")]
-        public Task PrivateHandlerWithAttribute(IDialogContext context, LuisResult luisResult)
-        {
-            throw new NotImplementedException();
-        }
-
-        [LuisIntent("PublicHandlerWithAttributeOne")]
-        [LuisIntent("PublicHandlerWithAttributeTwo")]
-        public Task PublicHandlerWithTwoAttributes(IDialogContext context, LuisResult luisResult)
-        {
-            throw new NotImplementedException();
-        }
-
-        private Task PublicHandlerWithNoAttribute(IDialogContext context, LuisResult luisResult)
-        {
-            throw new NotImplementedException();
-        }
-
-        private Task PrivateHandlerWithNoAttribute(IDialogContext context, LuisResult luisResult)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task PublicHandlerWithCovariance(IDialogContext context, object luisResult)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DoesNotMatchReturnType(IDialogContext context, LuisResult luisResult)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DoesNotMatchArgumentType(IDialogContext context, int notLuisResult)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     [TestClass]
     public sealed class LuisTests
     {
+        public sealed class DerivedLuisDialog : LuisDialog<object>
+        {
+            public DerivedLuisDialog(ILuisService service)
+                : base(service)
+            {
+            }
+
+            [LuisIntent("PublicHandlerWithAttribute")]
+            public Task PublicHandlerWithAttribute(IDialogContext context, LuisResult luisResult)
+            {
+                throw new NotImplementedException();
+            }
+
+            [LuisIntent("PrivateHandlerWithAttribute")]
+            public Task PrivateHandlerWithAttribute(IDialogContext context, LuisResult luisResult)
+            {
+                throw new NotImplementedException();
+            }
+
+            [LuisIntent("PublicHandlerWithAttributeOne")]
+            [LuisIntent("PublicHandlerWithAttributeTwo")]
+            public Task PublicHandlerWithTwoAttributes(IDialogContext context, LuisResult luisResult)
+            {
+                throw new NotImplementedException();
+            }
+
+            private Task PublicHandlerWithNoAttribute(IDialogContext context, LuisResult luisResult)
+            {
+                throw new NotImplementedException();
+            }
+
+            private Task PrivateHandlerWithNoAttribute(IDialogContext context, LuisResult luisResult)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task PublicHandlerWithCovariance(IDialogContext context, object luisResult)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void DoesNotMatchReturnType(IDialogContext context, LuisResult luisResult)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void DoesNotMatchArgumentType(IDialogContext context, int notLuisResult)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
         [TestMethod]
         public void All_Handlers_Are_Found()
         {
             var service = new Mock<ILuisService>();
-
             var dialog = new DerivedLuisDialog(service.Object);
-
             var handlers = LuisDialog.EnumerateHandlers(dialog).ToArray();
-
             Assert.AreEqual(7, handlers.Length);
+        }
+
+        public sealed class InvalidLuisDialog : LuisDialog<object>
+        {
+            public InvalidLuisDialog(ILuisService service)
+                : base(service)
+            {
+            }
+
+            [LuisIntent("HasAttributeButDoesNotMatchReturnType")]
+            public void HasAttributeButDoesNotMatchReturnType(IDialogContext context, LuisResult luisResult)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidIntentHandlerException))]
+        public void Invalid_Handle_Throws_Error()
+        {
+            var service = new Mock<ILuisService>();
+            var dialog = new InvalidLuisDialog(service.Object);
+            var handlers = LuisDialog.EnumerateHandlers(dialog).ToArray();
         }
     }
 }
