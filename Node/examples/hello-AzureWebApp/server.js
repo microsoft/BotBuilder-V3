53398bb895e701e3554e0dd5f5968673b8e6e1cc
Steven Ickman <stevenic@microsoft.com>
2016-04-06 13:39:18 -0700

    Added hello-AzureWebApp example for VS Studio.

---
+++ Node/examples/hello-AzureWebApp/server.js
@@ -0,0 +1,21 @@
+﻿var restify = require('restify');
+var builder = require('botbuilder');
+
+// Get secrets from server environment
+var botConnectorOptions = {
+    appId: process.env.BOTFRAMEWORK_APPID, 
+    appSecret: process.env.BOTFRAMEWORK_APPSECRET
+};
+
+// Create bot and add dialogs
+var bot = new builder.BotConnectorBot(botConnectorOptions);
+bot.add('/', function (session) {
+    session.send('Hello World');
+});
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 1337, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
