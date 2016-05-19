eb234216548735eba16a5d1e4a403fc685372604
andreo@microsoft.com <andreo@microsoft.com>
2016-05-19 13:04:09 -0700

    added incremental option to build file.

--- build-docs.cmd
+++ build-docs.cmd
@@ -3,7 +3,15 @@ IF /i [%1] == [local] GOTO :local
 IF /i [%1] == [fuseserver01] GOTO :fuseserver01
 ECHO usage:
 ECHO To build the Jekyll site and debug locally "build-docs local"
+ECHO To build the Jekyll site and debug locally "build-docs local-incremental"
 ECHO To build the jekyll site and copy it to http://fuseserver01/ "build-docs fuseserver01"
+GOTO :end
+
+:local
+cls
+ECHO [Building docs and serving them locally]
+ECHO [Wait until the localhost url is ready (it might take a while), then copy the url and visit it with the browser]
+call bundle exec jekyll serve --watch
 GOTO :end
 
 :local
