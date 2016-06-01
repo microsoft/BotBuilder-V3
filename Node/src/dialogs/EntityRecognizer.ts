799915fc644ca5cefbd709d8b417c519678f431c
Steven Ickman <stevenic@microsoft.com>
2016-06-01 10:43:03 -0700

    Added similar entities protection to findAllEntities()

--- Node/src/dialogs/EntityRecognizer.ts
+++ Node/src/dialogs/EntityRecognizer.ts
@@ -77,7 +77,7 @@ export class EntityRecognizer {
 
     static findAllEntities(entities: IEntity[], type: string): IEntity[] {
         var found: IEntity[] = [];
-        for (var i = 0; i < entities.length; i++) {
+        for (var i = 0; entities && i < entities.length; i++) {
             if (entities[i].type == type) {
                 found.push(entities[i]);
             }
