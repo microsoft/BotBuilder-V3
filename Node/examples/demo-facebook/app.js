ea7ae599c61bbe6eb003c35b8235cf9012fa4e2a
Muly Oved <mulyoved@gmail.com>
2016-06-02 13:20:37 +0300

    Change "port" to "PORT" fix deploy to Heroku
    http://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of

--- Node/examples/demo-facebook/app.js
+++ Node/examples/demo-facebook/app.js
@@ -34,7 +34,7 @@ var bot = new builder.BotConnectorBot({
 });
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
