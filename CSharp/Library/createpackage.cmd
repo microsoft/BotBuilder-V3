90b8ef17d8d025402e586c8e2606ccc1786961b3
Chris McConnell <chrimc@microsoft.com>
2016-06-20 15:23:00 -0700

    Fix nuget issues.

--- CSharp/Library/createpackage.cmd
+++ CSharp/Library/createpackage.cmd
@@ -5,6 +5,7 @@ setlocal enableextensions
 set errorlevel=0
 erase /s *.nupkg
 msbuild /property:Configuration=release Microsoft.Bot.Builder.csproj 
+msbuild /property:Configuration=release Microsoft.Bot.Builder.FormFlow.Json\Microsoft.Bot.Builder.FormFlow.Json.csproj
 msbuild /property:Configuration=release ..\tools\rview\rview.csproj
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.dll).FileVersionInfo.FileVersion"') do set version=%%v
 ..\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe pack Microsoft.Bot.Builder.nuspec -symbols -properties version=%version%
