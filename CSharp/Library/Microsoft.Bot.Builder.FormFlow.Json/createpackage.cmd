f5335a4e99ba284069d30ece503b83f6626006b7
Shahin Shayandeh <shahins@microsoft.com>
2016-07-05 21:07:07 -0700

    Update nuspec and createpackage.cmd for builder extensions

--- CSharp/Library/Microsoft.Bot.Builder.FormFlow.Json/createpackage.cmd
+++ CSharp/Library/Microsoft.Bot.Builder.FormFlow.Json/createpackage.cmd
@@ -5,6 +5,7 @@ setlocal enableextensions
 set errorlevel=0
 erase /s *.nupkg
 msbuild /property:Configuration=release Microsoft.Bot.Builder.FormFlow.Json.csproj
-for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.dll).FileVersionInfo.FileVersion"') do set version=%%v
-..\..\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe pack Microsoft.Bot.Builder.FormFlow.Json.nuspec -symbols -properties version=%version%
+for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.FormFlow.Json.dll).FileVersionInfo.FileVersion"') do set version=%%v
+for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.dll).FileVersionInfo.FileVersion"') do set builder=%%v
+..\..\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe pack Microsoft.Bot.Builder.FormFlow.Json.nuspec -symbols -properties version=%version%;builder=%builder%
 
