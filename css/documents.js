c675ad1b47dfc3257863ae7e29d05171ad4bae2a
andreo@microsoft.com <andreo@microsoft.com>
2016-07-09 13:56:28 -0700

    search proxy url fix

--- css/documents.js
+++ css/documents.js
@@ -134,10 +134,7 @@ $(function () {
         $('.post-title').html('Search results for \'' + data["q"] + '\'');
         $('#search-progress').addClass("loading");
         // Establish the data to pass to the proxy.
-        // var host = 'http://ic-devportal-local.azurewebsites.net/api/docssearch';
-        // var host = 'https://ic-devportal-scratch.ic-ase-internal.p.azurewebsites.net/api/docssearch';
         var host = 'https://dev.botframework.com/api/docssearch';
-        // var host = 'https://bingproxytestandreo.azurewebsites.net/api/search/';
         // Calls the proxy, passing the query, service operation and market.
         $.ajax({
             url: host,
