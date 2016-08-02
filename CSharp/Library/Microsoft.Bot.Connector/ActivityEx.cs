b54716008acd30248bd645a2e3bf8bfd4191f453
Alberto <apuyana@anuracode.com>
2016-08-01 20:10:22 -0500

    Include ChannelId when creating replay message. (#743)
    * Include ChannelId when creating replay message.
    
    * Add service URL when creating a replay message.

--- CSharp/Library/Microsoft.Bot.Connector/ActivityEx.cs
+++ CSharp/Library/Microsoft.Bot.Connector/ActivityEx.cs
@@ -35,13 +35,15 @@ namespace Microsoft.Bot.Connector
             reply.From = this.Recipient;
             reply.Recipient = this.From;
             reply.ReplyToId = this.Id;
+            reply.ServiceUrl = this.ServiceUrl;
+            reply.ChannelId = this.ChannelId;
             reply.Conversation = this.Conversation;
             reply.Conversation.IsGroup = null; // don't need to send in a reply
             reply.Text = text ?? String.Empty;
             reply.Locale = locale ?? this.Locale;
             return reply;
         }
-                
+
         public static IMessageActivity CreateMessageActivity() { return new Activity(ActivityTypes.Message); }
 
         public static IContactRelationUpdateActivity CreateContactRelationUpdateActivity() { return new Activity(ActivityTypes.ContactRelationUpdate); }
