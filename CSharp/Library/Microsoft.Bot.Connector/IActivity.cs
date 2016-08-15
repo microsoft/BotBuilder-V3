560a3490a29098d2196463b51b85e0955a33939a
Shahin Shayandeh <shahins@microsoft.com>
2016-08-15 16:01:24 -0700

    Move ChannelData to IActivity

--- CSharp/Library/Microsoft.Bot.Connector/IActivity.cs
+++ CSharp/Library/Microsoft.Bot.Connector/IActivity.cs
@@ -51,6 +51,24 @@ namespace Microsoft.Bot.Connector
         /// Bot's address 
         /// </summary>
         ChannelAccount Recipient { get; set; }
+
+        /// <summary>
+        /// Channel specific payload
+        /// </summary>
+        /// <remarks>
+        /// Some channels will provide channel specific data.
+        /// 
+        /// For a message originating in the channel it might provide the original native schema object for the channel. 
+        /// 
+        /// For a message coming into the channel it might accept a payload allowing you to create a "native" response for the channel.
+        /// 
+        /// Example:
+        /// * Email - The Email Channel will put the original Email metadata into the ChannelData object for outgoing messages, and will accep
+        /// on incoming message a Subject property, and a HtmlBody which can contain Html.  
+        /// 
+        /// The channel data essentially allows a bot to have access to native functionality on a per channel basis.
+        /// </remarks>
+        dynamic ChannelData { get; set; }
     }
 
 
--- CSharp/Library/Microsoft.Bot.Connector/IMessageActivity.cs
+++ CSharp/Library/Microsoft.Bot.Connector/IMessageActivity.cs
@@ -51,25 +51,6 @@ namespace Microsoft.Bot.Connector
         IList<Entity> Entities { get; set; }
 
         /// <summary>
-        /// Channel specific payload
-        /// </summary>
-        /// <remarks>
-        /// Some channels will provide channel specific data.
-        /// 
-        /// For a message originating in the channel it might provide the original native schema object for the channel. 
-        /// 
-        /// For a message coming into the channel it might accept a payload allowing you to create a "native" response for the channel.
-        /// 
-        /// Example:
-        /// * Email - The Email Channel will put the original Email metadata into the ChannelData object for outgoing messages, and will accep
-        /// on incoming message a Subject property, and a HtmlBody which can contain Html.  
-        /// 
-        /// The channel data essentially allows a bot to have access to native functionality on a per channel basis.
-        /// </remarks>
-        dynamic ChannelData { get; set; }
-
-
-        /// <summary>
         /// the original id this message is a response to
         /// </summary>
         string ReplyToId { get; set; }
