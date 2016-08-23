6fe9ec0fbacbbbad1d081b45cd04331524d2ca66
Kuznetsov Victor <kuznetsov.va@gmail.com>
2016-08-23 20:01:32 +0300

    StateClient constructor comments fixed. (#1026)

--- CSharp/Library/Microsoft.Bot.Connector/StateClientEx.cs
+++ CSharp/Library/Microsoft.Bot.Connector/StateClientEx.cs
@@ -9,7 +9,7 @@ namespace Microsoft.Bot.Connector
         /// <summary>
         /// Create a new instance of the StateClient class
         /// </summary>
-        /// <param name="baseUri">Base URI for the Connector service</param>
+        /// <param name="baseUri">Base URI for the State service</param>
         /// <param name="microsoftAppId">Optional. Your Microsoft app id. If null, this setting is read from settings["MicrosoftAppId"]</param>
         /// <param name="microsoftAppPassword">Optional. Your Microsoft app password. If null, this setting is read from settings["MicrosoftAppPassword"]</param>
         /// <param name="handlers">Optional. The delegating handlers to add to the http client pipeline.</param>
@@ -21,7 +21,7 @@ namespace Microsoft.Bot.Connector
         /// <summary>
         /// Create a new instance of the StateClient class
         /// </summary>
-        /// <param name="baseUri">Base URI for the Connector service</param>
+        /// <param name="baseUri">Base URI for the State service</param>
         /// <param name="credentials">Credentials for the Connector service</param>
         /// <param name="addJwtTokenRefresher">True, if JwtTokenRefresher should be included; False otherwise.</param>
         /// <param name="handlers">Optional. The delegating handlers to add to the http client pipeline.</param>
@@ -34,14 +34,14 @@ namespace Microsoft.Bot.Connector
         /// <summary>
         /// Create a new instance of the StateClient class
         /// </summary>
-        /// <remarks> This constructor will use http://api.botframework.com as the baseUri</remarks>
+        /// <remarks> This constructor will use https://state.botframework.com as the baseUri</remarks>
         /// <param name="credentials">Credentials for the Connector service</param>
         /// <param name="addJwtTokenRefresher">True, if JwtTokenRefresher should be included; False otherwise.</param>
         /// <param name="handlers">Optional. The delegating handlers to add to the http client pipeline.</param>
         public StateClient(MicrosoftAppCredentials credentials, bool addJwtTokenRefresher = true, params DelegatingHandler[] handlers)
             :this(addJwtTokenRefresher ? AddJwtTokenRefresher(handlers, credentials) : handlers)
         {
-            this.Credentials = credentials; 
+            this.Credentials = credentials;
         }
 
         private static DelegatingHandler[] AddJwtTokenRefresher(DelegatingHandler[] srcHandlers, MicrosoftAppCredentials credentials)
