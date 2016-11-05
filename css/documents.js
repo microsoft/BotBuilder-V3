c9f2d6d6b73395d8ccbd5ba94d3fbf404379d727
Andrea Orimoto <andreo@microsoft.com>
2016-11-04 17:52:52 -0700

    updated css to latest

--- css/documents.js
+++ css/documents.js
@@ -40,7 +40,7 @@ $( document ).ready(function() {
     var allTabsInPage = $('[id^="thetabs"]');
     activateAllTabs(allTabsInPage);
 
-    $(".brand-primary").after('<div class="upgrade-message"><span>There\'s a new version of the Microsoft Bot Framework. Update your bot now to use cards, carousels and action buttons. </span><a href="https://aka.ms/bf-migrate"><span>Learn how</span></a></div>');
+    $(".brand-primary").after('<div class="home-intro"><div class="upgrade-message"><span>There\'s a new version of the Microsoft Bot Framework. Update your bot now to use cards, carousels and action buttons. </span><a href="https://aka.ms/bf-migrate"><span>Learn how</span></a></div></div>');
     
 });
 
