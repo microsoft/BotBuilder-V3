749b356c69c5864a0a2bdc0761497b31c094a8d0
Steven Ickman <stevenic@microsoft.com>
2016-11-13 21:13:07 -0800

    Added new cancelAction demo

---
+++ Node/examples/feature-cancelAction/app.js
@@ -0,0 +1,52 @@
+/*-----------------------------------------------------------------------------
+This Bot demonstrates how to add a trigger action to a dialog that will cause 
+it to automatically be pushed onto the dialog stack based on a users utterance.
+
+# RUN THE BOT:
+
+    Run the bot from the command line using "node app.js" and then type 
+    "hello" to wake the bot up.
+    
+-----------------------------------------------------------------------------*/
+
+var builder = require('../../core/');
+
+var connector = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(connector);
+
+// Add default dialog
+bot.dialog('/', [
+    function (session) {
+        session.beginDialog('/listBuilder');
+    },
+    function (session, results) {
+        if (results.response) {
+            session.send(printList(results.response));
+        }
+    }
+]);
+
+bot.dialog('/listBuilder', function (session) {
+    if (!session.dialogData.list) {
+        session.dialogData.list = [];
+        session.send("Each message will added as a new item to the list.\nSay 'end list' when finished or 'cancel' to discard the list.\n")
+    } else {
+        session.dialogData.list.push(session.message.text);
+        session.save();
+    }
+}).cancelAction('cancelList', "List canceled", { 
+    matches: /^cancel/i 
+}).cancelAction('endList', null, {
+    matches: /^end list/i,
+    onSelectAction: function (session) {
+        session.endDialogWithResult({ response: session.dialogData.list });
+    }
+});
+
+function printList(list) {
+    var msg = "\nHere's your list:";
+    for (var i = 0; i < list.length; i++) {
+        msg += '\n* ' + list[i];
+    }
+    return msg;
+}
