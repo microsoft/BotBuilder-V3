30c2e25d1767117aeaa77e32d6e6984eadeff27a
Steven Ickman <stevenic@microsoft.com>
2016-11-21 14:10:52 -0800

    Added a new basics-customChoicePrompt example.

---
+++ Node/examples/basics-customChoicePrompt/app.js
@@ -0,0 +1,36 @@
+/*-----------------------------------------------------------------------------
+This Bot demonstrates how to create a custom prompt the conditionally uses a 
+choice() prompt. The user can either pick an option from the list of choices 
+or enter a new choice.  The sample also shows how to prompt the user to add
+their choice to the list.
+
+# RUN THE BOT:
+
+    Run the bot from the command line using "node app.js" and then type 
+    "hello" to wake the bot up.
+    
+-----------------------------------------------------------------------------*/
+
+var builder = require('../../core/');
+var savedListPrompt = require('./savedListPrompt');
+
+var connector = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(connector);
+
+// Create prompts
+savedListPrompt.create(bot);
+
+// Root dialog
+bot.dialog('/', [
+    function (session) {
+        // Prompt for message to send
+        savedListPrompt.beginDialog(session, {
+            field: 'savedMessages',
+            choicesPrompt: "What message would you like to send? Choose a saved message from the list or enter a new one.",
+            noChoicesPrompt: "What message would you like to send?"
+        });
+    },
+    function (session, results) {
+        session.send("Sending message: " + results.response);
+    }
+]);
