f2c93413ef01dfaecbe6e874196a75f7318b760a
Steve Ickman <stevenic@microsoft.com>
2017-01-05 11:45:18 -0800

    Pushed 3.5.1

--- Node/core/lib/dialogs/EntityRecognizer.js
+++ Node/core/lib/dialogs/EntityRecognizer.js
@@ -199,8 +199,8 @@ var EntityRecognizer = (function () {
         }
     };
     EntityRecognizer.dateExp = /^\d{4}-\d{2}-\d{2}/i;
-    EntityRecognizer.yesExp = /^(1|y|yes|yep|sure|ok|true)/i;
-    EntityRecognizer.noExp = /^(2|n|no|nope|not|false)/i;
+    EntityRecognizer.yesExp = /^(1|y|yes|yep|sure|ok|true)(\W|$)/i;
+    EntityRecognizer.noExp = /^(2|n|no|nope|not|false)(\W|$)/i;
     EntityRecognizer.numberExp = /[+-]?(?:\d+\.?\d*|\d*\.?\d+)/;
     EntityRecognizer.ordinalWords = 'first|second|third|fourth|fifth|sixth|seventh|eigth|ninth|tenth';
     return EntityRecognizer;
