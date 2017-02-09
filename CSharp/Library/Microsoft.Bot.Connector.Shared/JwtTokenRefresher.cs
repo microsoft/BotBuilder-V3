8687971564ce30d8099a8be12e0255d66bd1b5d8
Will Portnoy <wportnoy@microsoft.com>
2017-02-09 15:17:12 -0800

    add comments to JwtTokenRefresher, unfold Unauthorized method

--- CSharp/Library/Microsoft.Bot.Connector.Shared/JwtTokenRefresher.cs
+++ CSharp/Library/Microsoft.Bot.Connector.Shared/JwtTokenRefresher.cs
@@ -24,15 +24,23 @@ namespace Microsoft.Bot.Connector
         {
             HttpResponseMessage response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
 
+            // possibly a transient "token expiration" failure
             if (response.StatusCode == HttpStatusCode.Unauthorized)
             {
                 response.Dispose();
+                // this call might throw if the Microsoft login service returns an oauth failure
                 var token = await credentials.GetTokenAsync(true).ConfigureAwait(false);
+                // adds token to outgoing request
                 await credentials.ProcessHttpRequestAsync(request, cancellationToken).ConfigureAwait(false);
+                // retry request with refreshed token
                 response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
             }
 
-            if (this.Unauthorized(response.StatusCode))
+            // since we've ruled out transient "token expiration" failure, or we've found a permanent Forbidden failure
+            // this failure will come from a downstream system like channel connector or state service rather than the Microsoft login service
+            // then throw an exception with additional context
+            // this centralizes the handling for this StatusCode here rather than the autorest-generated clients
+            if (response.StatusCode == HttpStatusCode.Forbidden || response.StatusCode == HttpStatusCode.Unauthorized)
             {
                 using (response)
                 {
@@ -50,11 +58,6 @@ namespace Microsoft.Bot.Connector
             }
 
             return response;
-        }
-
-        private bool Unauthorized(System.Net.HttpStatusCode code)
-        {
-            return code == HttpStatusCode.Forbidden || code == HttpStatusCode.Unauthorized;
         }
     }
 }
