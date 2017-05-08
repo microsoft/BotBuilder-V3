5bbe0ffa5894738f9594bb4d60beb32e01c14400
Shahin Shayandeh <shahins@microsoft.com>
2017-05-08 11:08:57 -0700

    Add CustomRedirectHandler to handle 307 redirects

--- CSharp/Library/Microsoft.Bot.Connector.Shared/ConnectorClientEx.cs
+++ CSharp/Library/Microsoft.Bot.Connector.Shared/ConnectorClientEx.cs
@@ -31,18 +31,32 @@ namespace Microsoft.Bot.Connector
         /// <param name="addJwtTokenRefresher">True, if JwtTokenRefresher should be included; False otherwise.</param>
         /// <param name="handlers">Optional. The delegating handlers to add to the http client pipeline.</param>
         public ConnectorClient(Uri baseUri, MicrosoftAppCredentials credentials, bool addJwtTokenRefresher = true, params DelegatingHandler[] handlers)
-            : this(baseUri, addJwtTokenRefresher ? AddJwtTokenRefresher(handlers, credentials) : handlers)
+            : this(baseUri, addJwtTokenRefresher ? AddJwtTokenRefresher(handlers, credentials): handlers)
         {
             this.Credentials = credentials;
         }
 
+        /// <summary>
+        /// Create a new instances of the ConnectorClient.
+        /// </summary>
+        /// <param name="baseUri">Base URI for the Connector service</param>
+        /// <param name="credentials">Credentials for the Connector service</param>
+        /// <param name="httpClientHandler">The httpClientHandler used by http client</param>
+        /// <param name="addJwtTokenRefresher">True, if JwtTokenRefresher should be included; False otherwise.</param>
+        /// <param name="handlers">Optional. The delegating handlers to add to the http client pipeline.</param>
+        public ConnectorClient(Uri baseUri, MicrosoftAppCredentials credentials, HttpClientHandler httpClientHandler, bool addJwtTokenRefresher = true, params DelegatingHandler[] handlers)
+            : this(baseUri, httpClientHandler, addJwtTokenRefresher ? AddJwtTokenRefresher(handlers, credentials) : handlers)
+        {
+            this.Credentials = credentials;
+        }
+        
         private static DelegatingHandler[] AddJwtTokenRefresher(DelegatingHandler[] srcHandlers, MicrosoftAppCredentials credentials)
         {
             var handlers = new List<DelegatingHandler>(srcHandlers);
             handlers.Add(new JwtTokenRefresher(credentials));
             return handlers.ToArray();
         }
-
+        
         // client defaults to sending the expect: continue header, which isn't very efficient, 
         partial void CustomInitialize()
         {
