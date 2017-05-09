dcf1574facf7fb3b745d45004256b6a4d9ac97b1
Eric Dahlvang <v-erdahl@microsoft.com>
2017-05-09 12:57:50 -0700

    [C# SDK] Add IEquatable to Attachment and Do not respond to Ping in SimpleDispatchDialog sample (#2687)
    * Added IEquatable to Attachment
    
    * Do not respond to Ping in SimpleDispatchDialog sample.

---
+++ CSharp/Library/Microsoft.Bot.Connector.Shared/AttachmentEx.cs
@@ -0,0 +1,34 @@
+﻿namespace Microsoft.Bot.Connector
+{
+    using System;
+
+    public partial class Attachment : IEquatable<Attachment>
+    {
+        public bool Equals(Attachment other)
+        {
+            return other != null
+                && this.ContentType == other.ContentType
+                && this.ContentUrl == other.ContentUrl
+                && object.Equals(this.Content, other.Content)
+                && object.Equals(this.Name, other.Name)
+                && object.Equals(this.ThumbnailUrl, other.ThumbnailUrl);
+
+        }
+
+        public override bool Equals(object other)
+        {
+            return this.Equals(other as Attachment);
+        }
+
+        public override int GetHashCode()
+        {
+            var code = this.ContentType.GetHashCode()
+                ^ this.ContentUrl.GetHashCode()
+                ^ (this.Content == null ? 13 : this.Content.GetHashCode())
+                ^ (this.Name == null ? 17 : this.Name.GetHashCode())
+                ^ (this.ThumbnailUrl == null ? 23 : this.ThumbnailUrl.GetHashCode());
+
+            return code;
+        }
+    }
+}
