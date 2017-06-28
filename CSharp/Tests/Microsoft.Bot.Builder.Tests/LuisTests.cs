microsoft/BotBuilder-V3

commit b6cd3ff85e8dc57ac586a11db489a0c75c635ae2

handle null IMessageActivity.Text as empty intent in LuisDialog

CSharp/Tests/Microsoft.Bot.Builder.Tests/LuisTests.cs
@@ -301,6 +301,42 @@ namespace Microsoft.Bot.Builder.Tests
             }
         }
 
+        [Serializable]
+        public sealed class NullMessageTextLuisDialog : LuisDialog<object>
+        {
+            public NullMessageTextLuisDialog(params ILuisService[] services)
+                : base(services)
+            {
+            }
+
+            [LuisIntent("")]
+            public async Task NullHandler(IDialogContext context, LuisResult luisResult)
+            {
+                await context.PostAsync("I see null");
+                context.Wait(MessageReceived);
+            }
+        }
+
+        [TestMethod]
+        public async Task NullMessageText_Is_EmptyIntent()
+        {
+            var service = new Mock<ILuisService>();
+
+            var dialog = new NullMessageTextLuisDialog(service.Object);
+
+            using (new FiberTestBase.ResolveMoqAssembly(service.Object))
+            using (var container = Build(Options.ResolveDialogFromContainer, service.Object))
+            {
+                var builder = new ContainerBuilder();
+                builder
+                    .RegisterInstance(dialog)
+                    .As<IDialog<object>>();
+                builder.Update(container);
+
+                await AssertScriptAsync(container, null, "I see null");
+            }
+        }
+
 
         [TestMethod]
         public async Task Service_With_LuisActionDialog()

CSharp/Library/Microsoft.Bot.Builder/Dialogs/LuisDialog.cs
@@ -209,34 +209,43 @@ namespace Microsoft.Bot.Builder.Dialogs
             var message = await item;
             var messageText = await GetLuisQueryTextAsync(context, message);
 
-            // Modify request by the service to add attributes and then by the dialog to reflect the particular query
-            var tasks = this.services.Select(s => s.QueryAsync(ModifyLuisRequest(s.ModifyRequest(new LuisRequest(messageText))), context.CancellationToken)).ToArray();
-            var results = await Task.WhenAll(tasks);
-
-            var winners = from result in results.Select((value, index) => new { value, index })
-                          let resultWinner = BestIntentFrom(result.value)
-                          where resultWinner != null
-                          select new LuisServiceResult(result.value, resultWinner, this.services[result.index]);
-
-            var winner = this.BestResultFrom(winners);
-
-            if (winner == null)
+            if (messageText != null)
             {
-                throw new InvalidOperationException("No winning intent selected from Luis results.");
-            }
+                // Modify request by the service to add attributes and then by the dialog to reflect the particular query
+                var tasks = this.services.Select(s => s.QueryAsync(ModifyLuisRequest(s.ModifyRequest(new LuisRequest(messageText))), context.CancellationToken)).ToArray();
+                var results = await Task.WhenAll(tasks);
 
-            if (winner.Result.Dialog?.Status == DialogResponse.DialogStatus.Question)
-            {
+                var winners = from result in results.Select((value, index) => new { value, index })
+                              let resultWinner = BestIntentFrom(result.value)
+                              where resultWinner != null
+                              select new LuisServiceResult(result.value, resultWinner, this.services[result.index]);
+
+                var winner = this.BestResultFrom(winners);
+
+                if (winner == null)
+                {
+                    throw new InvalidOperationException("No winning intent selected from Luis results.");
+                }
+
+                if (winner.Result.Dialog?.Status == DialogResponse.DialogStatus.Question)
+                {
 #pragma warning disable CS0618
-                var childDialog = await MakeLuisActionDialog(winner.LuisService,
-                                                             winner.Result.Dialog.ContextId,
-                                                             winner.Result.Dialog.Prompt);
+                    var childDialog = await MakeLuisActionDialog(winner.LuisService,
+                                                                 winner.Result.Dialog.ContextId,
+                                                                 winner.Result.Dialog.Prompt);
 #pragma warning restore CS0618
-                context.Call(childDialog, LuisActionDialogFinished);
+                    context.Call(childDialog, LuisActionDialogFinished);
+                }
+                else
+                {
+                    await DispatchToIntentHandler(context, item, winner.BestIntent, winner.Result);
+                }
             }
             else
             {
-                await DispatchToIntentHandler(context, item, winner.BestIntent, winner.Result);
+                var intent = new IntentRecommendation() { Intent = string.Empty, Score = 1.0 };
+                var result = new LuisResult() { TopScoringIntent = intent };
+                await DispatchToIntentHandler(context, item, intent, result);
             }
         }
 

CSharp/Library/Microsoft.Bot.Builder/Luis/Models/LuisResult.cs
@@ -9,11 +9,9 @@
 namespace Microsoft.Bot.Builder.Luis.Models
 {
     using System;
-    using System.Linq;
     using System.Collections.Generic;
-    using Newtonsoft.Json;
     using Microsoft.Rest;
-    using Microsoft.Rest.Serialization;
+    using Newtonsoft.Json;
 
     public partial class LuisResult
     {
@@ -51,19 +49,19 @@ namespace Microsoft.Bot.Builder.Luis.Models
         /// The intents found in the query text.
         /// </summary>
         [JsonProperty(PropertyName = "intents")]
-        public IList<IntentRecommendation> Intents { get; set; }
+        public IList<IntentRecommendation> Intents { get; set; } = Array.Empty<IntentRecommendation>();
 
         /// <summary>
         /// The entities found in the query text.
         /// </summary>
         [JsonProperty(PropertyName = "entities")]
-        public IList<EntityRecommendation> Entities { get; set; }
+        public IList<EntityRecommendation> Entities { get; set; } = Array.Empty<EntityRecommendation>();
 
         /// <summary>
         /// The composite entities found in the utterance.
         /// </summary>
         [JsonProperty(PropertyName = "compositeEntities")]
-        public IList<CompositeEntity> CompositeEntities { get; set; }
+        public IList<CompositeEntity> CompositeEntities { get; set; } = Array.Empty<CompositeEntity>();
 
         /// <summary>
         /// </summary>

CSharp/Tests/Microsoft.Bot.Sample.Tests/SimpleAlarmBotTests.cs
@@ -33,23 +33,16 @@
 
 using System;
 using System.Globalization;
-using System.Linq.Expressions;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
-
-using Microsoft.Bot.Connector;
-using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Builder.Luis;
-using Microsoft.Bot.Builder.Dialogs.Internals;
-using Microsoft.Bot.Builder.Tests;
-using Microsoft.Bot.Builder.Luis.Models;
-
-using Moq;
 using Autofac;
-
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Tests;
 using Microsoft.Bot.Sample.SimpleAlarmBot;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Microsoft.Bot.Sample.Tests
 {
@@ -69,6 +62,7 @@ namespace Microsoft.Bot.Sample.Tests
 
             Func<IDialog<object>> MakeRoot = () => new SimpleAlarmDialog(luis.Object);
             var toBot = MakeTestMessage();
+            toBot.Text = string.Empty;
 
             using (new FiberTestBase.ResolveMoqAssembly(luis.Object))
             using (var container = Build(Options.ScopedQueue, luis.Object))

commit 07f66d7e43de9d4fb8ef3c31f78b9b575e3ee272

DispatchDialog depends on LuisResult.Intents backfill from LuisResult.TopScoringIntent but https://github.com/Microsoft/BotBuilder/commit/b6cd3ff85e8dc57ac586a11db489a0c75c635ae2 broke it.

CSharp/Library/Microsoft.Bot.Builder/Luis/LuisService.cs
@@ -237,6 +237,21 @@ namespace Microsoft.Bot.Builder.Luis
             return luisRequest.BuildUri(this.model);
         }
 
+        public static void Fix(LuisResult result)
+        {
+            // fix up Luis result for backward compatibility
+            // v2 api is not returning list of intents if verbose query parameter 
+            // is not set. This will move IntentRecommendation in TopScoringIntent
+            // to list of Intents.
+            if (result.Intents == null || result.Intents.Count == 0)
+            {
+                if (result.TopScoringIntent != null)
+                {
+                    result.Intents = new List<IntentRecommendation> { result.TopScoringIntent };
+                }
+            }
+        }
+
         async Task<LuisResult> ILuisService.QueryAsync(Uri uri, CancellationToken token)
         {
             string json;
@@ -250,14 +265,7 @@ namespace Microsoft.Bot.Builder.Luis
             try
             {
                 var result = JsonConvert.DeserializeObject<LuisResult>(json);
-                // fix up Luis result for backward compatibility
-                // v2 api is not returning list of intents if verbose query parameter 
-                // is not set. This will move IntentRecommendation in TopScoringIntent
-                // to list of Intents.
-                if (result.TopScoringIntent != null && result.Intents == null)
-                {
-                    result.Intents = new List<IntentRecommendation> { result.TopScoringIntent };
-                }
+                Fix(result);
                 return result;
             }
             catch (JsonException ex)

CSharp/Tests/Microsoft.Bot.Builder.Tests/LuisTests.cs
@@ -337,6 +337,20 @@ namespace Microsoft.Bot.Builder.Tests
             }
         }
 
+        [TestMethod]
+        public void NullOrEmptyIntents_DefaultsTo_TopScoringIntent()
+        {
+            var intent = new IntentRecommendation();
+            var result = new LuisResult()
+            {
+                TopScoringIntent = intent
+            };
+
+            LuisService.Fix(result);
+
+            Assert.AreEqual(1, result.Intents.Count);
+            Assert.AreEqual(intent, result.Intents[0]);
+        }
 
         [TestMethod]
         public async Task Service_With_LuisActionDialog()