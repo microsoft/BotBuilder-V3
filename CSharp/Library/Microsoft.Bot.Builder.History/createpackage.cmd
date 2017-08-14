68a37529312d8b6836df4f0f796f14e26e2a0b80
Shahin Shayandeh <shahins@microsoft.com>
2017-08-14 12:52:12 -0700

    Update nuget commandline tool to 4.1.0 for bot.builder.history

--- CSharp/Library/Microsoft.Bot.Builder.History/createpackage.cmd
+++ CSharp/Library/Microsoft.Bot.Builder.History/createpackage.cmd
@@ -9,6 +9,6 @@ erase /s ..\nuget\Microsoft.Bot.Builder.History*nupkg
 msbuild /property:Configuration=release Microsoft.Bot.Builder.History.csproj 
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.dll).FileVersionInfo.FileVersion"') do set builder=%%v
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.History.dll).FileVersionInfo.FileVersion"') do set version=%%v
-..\..\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe pack Microsoft.Bot.Builder.History.nuspec -symbols -properties version=%version%;builder=%builder% -OutputDirectory ..\nuget
+..\..\packages\NuGet.CommandLine.4.1.0\tools\NuGet.exe pack Microsoft.Bot.Builder.History.nuspec -symbols -properties version=%version%;builder=%builder% -OutputDirectory ..\nuget
 echo *** Finished building Microsoft.Bot.Builder.History
 
