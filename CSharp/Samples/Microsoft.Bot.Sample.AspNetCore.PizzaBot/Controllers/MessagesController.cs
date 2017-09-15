f08c77aa9906a1976c762b17cccfabe05debe917
forest93 <forest93@163.com>
2017-09-15 16:28:14 +0800

    Add PizzaBot (netcore20) sample.

---
+++ CSharp/Samples/Microsoft.Bot.Sample.AspNetCore.PizzaBot/Controllers/MessagesController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Bot.Sample.AspNetCore.PizzaBot.Controllers
+{
+
+    [Route("api/[controller]")]
+    [BotAuthentication]
+    public class MessagesController : Controller
+    {
+        private ILogger logger;
+
+        private static IForm<PizzaOrder> BuildForm()
+        {
+            var builder = new FormBuilder<PizzaOrder>();
+
+            ActiveDelegate<PizzaOrder> isBYO = (pizza) => pizza.Kind == PizzaOptions.BYOPizza;
+            ActiveDelegate<PizzaOrder> isSignature = (pizza) => pizza.Kind == PizzaOptions.SignaturePizza;
+            ActiveDelegate<PizzaOrder> isGourmet = (pizza) => pizza.Kind == PizzaOptions.GourmetDelitePizza;
+            ActiveDelegate<PizzaOrder> isStuffed = (pizza) => pizza.Kind == PizzaOptions.StuffedPizza;
+
+            return builder
+                // .Field(nameof(PizzaOrder.Choice))
+                .Field(nameof(PizzaOrder.Size))
+                .Field(nameof(PizzaOrder.Kind))
+                .Field("BYO.Crust", isBYO)
+                .Field("BYO.Sauce", isBYO)
+                .Field("BYO.Toppings", isBYO)
+                .Field(nameof(PizzaOrder.GourmetDelite), isGourmet)
+                .Field(nameof(PizzaOrder.Signature), isSignature)
+                .Field(nameof(PizzaOrder.Stuffed), isStuffed)
+                .AddRemainingFields()
+                .Confirm("Would you like a {Size}, {BYO.Crust} crust, {BYO.Sauce}, {BYO.Toppings} pizza?", isBYO)
+                .Confirm("Would you like a {Size}, {&Signature} {Signature} pizza?", isSignature, dependencies: new string[] { "Size", "Kind", "Signature" })
+                .Confirm("Would you like a {Size}, {&GourmetDelite} {GourmetDelite} pizza?", isGourmet)
+                .Confirm("Would you like a {Size}, {&Stuffed} {Stuffed} pizza?", isStuffed)
+                .Build()
+                ;
+        }
+
+        internal static IDialog<PizzaOrder> MakeRoot()
+        {
+            return Chain.From(() => new PizzaOrderDialog(BuildForm));
+        }
+
+        public MessagesController(ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null) throw new ArgumentNullException(nameof(loggerFactory));
+            this.logger = loggerFactory.CreateLogger<MessagesController>();
+        }
+
+        /// <summary>
+        /// POST: api/Messages
+        /// receive a message from a user and send replies
+        /// </summary>
+        /// <param name="activity"></param>
+        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        {
+            if (activity != null)
+            {
+                // one of these will have an interface and process it
+                switch (activity.GetActivityType())
+                {
+                    case ActivityTypes.Message:
+                        await Conversation.SendAsync(activity, MakeRoot);
+                        break;
+
+                    case ActivityTypes.ConversationUpdate:
+                    case ActivityTypes.ContactRelationUpdate:
+                    case ActivityTypes.Typing:
+                    case ActivityTypes.DeleteUserData:
+                    default:
+                        logger.LogWarning("Unknown activity type ignored: {0}", activity.GetActivityType());
+                        break;
+                }
+            }
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+        }
+    }
+}
