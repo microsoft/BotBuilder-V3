bf03b140724d7c42a8b2c41143e0cbe79340f376
forest93 <forest93@163.com>
2017-09-18 22:11:39 +0800

    Migrate Bot.Builder.Tests (net46). 16/217 tests failed.

---
+++ CSharp/Library/Microsoft.Bot.Builder.Autofac/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿// This file should only be included in VS2017 project file. VS2015 project file has Property/AssemblyInfo.cs.
+
+using System;
+using System.Runtime.CompilerServices;
+
+
+[assembly: InternalsVisibleTo("Microsoft.Bot.Builder.Tests")]
+[assembly: InternalsVisibleTo("Microsoft.Bot.Sample.Tests")]
+
