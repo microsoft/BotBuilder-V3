4158f659709fa7284c18ccaa636f587bef196458
forest93 <forest93@163.com>
2017-09-18 11:13:56 +0800

    Improve MemberInfoSerializationSurrogate. It should support generic methods now.

--- CSharp/Library/Microsoft.Bot.Builder/Fibers/NetStandardSerialization.cs
+++ CSharp/Library/Microsoft.Bot.Builder/Fibers/NetStandardSerialization.cs
@@ -79,19 +79,7 @@ namespace Microsoft.Bot.Builder.Internals.Fibers
 
             public void GetObjectData(object obj, SerializationInfo info, StreamingContext context)
             {
-                var member = (MemberInfo)obj;
-                info.SetType(typeof(MemberInfoReference));
-                info.AddValue("DeclaringType", new TypeSerializationSurrogate.TypeReference(member.DeclaringType));
-                info.AddValue("Name", member.Name);
-                info.AddValue("MemberType", member.MemberType);
-                if (obj is MethodBase method)
-                {
-                    var bindingFlags = default(BindingFlags);
-                    bindingFlags |= method.IsPublic ? BindingFlags.Public : BindingFlags.NonPublic;
-                    bindingFlags |= method.IsStatic ? BindingFlags.Static : BindingFlags.Instance;
-                    info.AddValue("BindingAttr", bindingFlags);
-                    info.AddValue("Parameters", method.GetParameters().Select(p => p.ParameterType).ToArray());
-                }
+                MemberInfoReference.GetObjectData((MemberInfo) obj, info, context);
             }
 
             public object SetObjectData(object obj, SerializationInfo info, StreamingContext context,
@@ -119,30 +107,86 @@ namespace Microsoft.Bot.Builder.Internals.Fibers
                 private readonly string Name = null;
                 private readonly MemberTypes MemberType = default(MemberTypes);
                 private readonly BindingFlags BindingAttr = default(BindingFlags);
+                private readonly Type[] GenericParameters = null;
                 private readonly Type[] Parameters = null;
+
+                private static BindingFlags GetBindingAttr(MemberInfo member)
+                {
+                    if (member == null) throw new ArgumentNullException(nameof(member));
+                    var bindingFlags = default(BindingFlags);
+                    switch (member)
+                    {
+                        case MethodBase method:
+                            bindingFlags |= method.IsPublic ? BindingFlags.Public : BindingFlags.NonPublic;
+                            bindingFlags |= method.IsStatic ? BindingFlags.Static : BindingFlags.Instance;
+                            break;
+                        case FieldInfo field:
+                            bindingFlags |= field.IsPublic ? BindingFlags.Public : BindingFlags.NonPublic;
+                            bindingFlags |= field.IsStatic ? BindingFlags.Static : BindingFlags.Instance;
+                            break;
+                        default:
+                            return BindingFlags.Public | BindingFlags.NonPublic
+                                   | BindingFlags.Static | BindingFlags.Instance;
+                    }
+                    return bindingFlags;
+                }
+
+                public static void GetObjectData(MemberInfo member, SerializationInfo info, StreamingContext context)
+                {
+                    info.SetType(typeof(MemberInfoReference));
+                    info.AddValue(nameof(DeclaringType), new TypeSerializationSurrogate.TypeReference(member.DeclaringType));
+                    info.AddValue(nameof(Name), member.Name);
+                    info.AddValue(nameof(MemberType), member.MemberType);
+                    info.AddValue(nameof(BindingAttr), GetBindingAttr(member));
+                    if (member is MethodBase method)
+                    {
+                        info.AddValue(nameof(GenericParameters),
+                            method.GetGenericArguments().ToArray());
+                        info.AddValue(nameof(Parameters),
+                            method.GetParameters().Select(p => p.ParameterType).ToArray());
+                    }
+                }
+
+                private bool MatchMethodSignature(MethodBase method)
+                {
+                    Debug.Assert(method.Name == Name);
+                    var gpa = method.GetGenericArguments();
+                    if (gpa.Length != GenericParameters.Length) return false;
+                    var pa = method.GetParameters();
+                    if (pa.Length != Parameters.Length) return false;
+                    if (gpa.Length > 0)
+                    {
+                        var genericMethod = ((MethodInfo) method).MakeGenericMethod(GenericParameters);
+                        pa = genericMethod.GetParameters();
+                    }
+                    for (int i = 0; i < pa.Length; i++)
+                    {
+                        if (pa[i].ParameterType != Parameters[i]) return false;
+                    }
+                    return true;
+                }
 
                 public object GetRealObject(StreamingContext context)
                 {
                     if (MemberType == MemberTypes.Method || MemberType == MemberTypes.Constructor)
                     {
-                        var bindingFlags = BindingAttr;
-                        var paramTypes = Parameters;
-                        var methods = DeclaringType.GetMember(Name, MemberType, bindingFlags);
-                        Debug.Assert(paramTypes.All(t => t != null), "Detected null argument type in the method signature.");
+                        var methods = DeclaringType.GetMember(Name, MemberType, BindingAttr | BindingFlags.DeclaredOnly);
+                        if (Parameters.Any(t => t == null))
+                            throw new ArgumentException("Detected null argument type in the method signature.",
+                                nameof(Parameters));
+                        if (Parameters.Any(t => t.IsGenericParameter))
+                            throw new ArgumentException("Detected generic parameter in the method signature.",
+                                nameof(Parameters));
                         try
                         {
-                            return methods.Cast<MethodBase>().First(m =>
-                                m.GetParameters().Select(p => p.ParameterType)
-                                    .SequenceEqual(paramTypes));
+                            return methods.Cast<MethodBase>().First(MatchMethodSignature);
                         }
                         catch (InvalidOperationException)
                         {
                             throw new MissingMethodException(DeclaringType.FullName, Name);
                         }
                     }
-                    var members = DeclaringType.GetMember(Name, MemberType,
-                        BindingFlags.Static | BindingFlags.Instance
-                        | BindingFlags.Public | BindingFlags.NonPublic);
+                    var members = DeclaringType.GetMember(Name, MemberType, BindingAttr | BindingFlags.DeclaredOnly);
                     if (members.Length == 0) throw new MissingMemberException(DeclaringType.FullName, Name);
                     if (members.Length > 1)
                         throw new AmbiguousMatchException($"Found multiple \"{Name}\" in \"{DeclaringType}\".");
