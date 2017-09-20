509c153f45292791c26a40e097ca0e546414a961
forest93 <forest93@163.com>
2017-09-20 14:16:05 +0800

    Add SimpleFacebookAuthBot (netcoreapp2.0). Yet to be refactored.

---
+++ CSharp/Samples/Microsoft.Bot.Sample.AspNetCore.SimpleFacebookAuthBot/Controllers/MessagesController.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Bot.Sample.AspNetCore.SimpleFacebookAuthBot.Controllers
+{
+    [Route("api/[controller]")]
+    [BotAuthentication]
+    public class MessagesController : Controller
+    {
+
+        private readonly ILogger<MessagesController> logger;
+
+        public MessagesController(ILoggerFactory loggerFactory)
+        {
+            logger = loggerFactory.CreateLogger<MessagesController>();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public virtual async Task<IActionResult> Post([FromBody]Activity activity)
+        {
+            if (activity != null)
+            {
+                // one of these will have an interface and process it
+                switch (activity.GetActivityType())
+                {
+                    case ActivityTypes.Message:
+                        await Conversation.SendAsync(activity, () => SimpleFacebookAuthDialog.dialog);
+                        break;
+
+                    case ActivityTypes.ConversationUpdate:
+                    case ActivityTypes.ContactRelationUpdate:
+                    case ActivityTypes.Typing:
+                    case ActivityTypes.DeleteUserData:
+                    default:
+                        logger.LogError($"Unknown activity type ignored: {activity.GetActivityType()}");
+                        break;
+                }
+            }
+            return StatusCode((int) HttpStatusCode.Accepted);
+        }
+    }
+}
---
+++ CSharp/Samples/Microsoft.Bot.Sample.AspNetCore.SimpleFacebookAuthBot/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Connector;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Bot.Sample.AspNetCore.SimpleFacebookAuthBot
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile("_private/appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // .NET Core specific: Inject configurations to the static configuration provider.
+            SettingsUtils.AttachConfiguration(Configuration);
+
+            services.AddSingleton(_ => Configuration);
+
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
