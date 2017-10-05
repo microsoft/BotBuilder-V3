1f0f28560c296147b4401989164baecb7335e894
Carlos Castro <carlosscastro@users.noreply.github.com>
2017-10-04 17:20:54 -0700

    Fix bugs in comments in C# SDK samples. InMemoryDataStore name was incorrect, and also there was a mix in autofact patterns for the samples that used Conversation.UpdateContainer(). (#3571)

--- CSharp/Samples/SimpleIVRBot/Global.asax.cs
+++ CSharp/Samples/SimpleIVRBot/Global.asax.cs
@@ -20,14 +20,13 @@ namespace SimpleIVRBot
             //            // Uncomment one of the lines below to choose your store
             //            // var store = new TableBotDataStore("...DataStorageConnectionString..."); // requires Microsoft.BotBuilder.Azure Nuget package 
             //            // var store = new DocumentDbBotDataStore("cosmos db uri", "cosmos db key"); // requires Microsoft.BotBuilder.Azure Nuget package 
-            //            // var store = new InMemoryStore(); // volatile in-memory store
+            //            // var store = new InMemoryDataStore(); // volatile in-memory store
 
             //            builder.Register(c => store)
             //                .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
             //                .AsSelf()
             //                .SingleInstance();
 
-            //            builder.Update(Conversation.Container);
             //        });
 
 
