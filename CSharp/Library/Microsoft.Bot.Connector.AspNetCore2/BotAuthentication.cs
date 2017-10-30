ba60581e887834629c6b917b2a1c7c419c35ceaf
Shahin Shayandeh <shahins@microsoft.com>
2017-10-30 15:06:29 -0700

    Add bot authentication support for asp net core 2 authentication middleware (#3687)
    * Add bot authentication support for asp net core 2 authentication pipeline
    
    * Remove app id and password from aspnetcore2 sample

---
+++ CSharp/Library/Microsoft.Bot.Connector.AspNetCore2/BotAuthentication.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.Bot.Connector
+{
+    public static class BotAuthenticationExtensions
+    {
+        public static AuthenticationBuilder AddBotAuthentication(this AuthenticationBuilder builder, string microsoftAppId, string microsoftAppPassword)
+            => builder.AddBotAuthentication(new StaticCredentialProvider(microsoftAppId, microsoftAppPassword));
+
+        public static AuthenticationBuilder AddBotAuthentication(this AuthenticationBuilder builder, ICredentialProvider credentialProvider)
+            => builder.AddBotAuthentication(JwtBearerDefaults.AuthenticationScheme, displayName: "botAuthenticator", configureOptions: options =>
+            {
+                options.CredentialProvider = credentialProvider;
+                options.Events = new JwtBearerEvents();
+            });
+        public static AuthenticationBuilder AddBotAuthentication(this AuthenticationBuilder builder, string authenticationScheme, string displayName, Action<BotAuthenticationOptions> configureOptions)
+        {
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IPostConfigureOptions<BotAuthenticationOptions>, JwtBearerPostConfigureOptions>());
+            return builder.AddScheme<BotAuthenticationOptions, BotAuthenticationHandler>(authenticationScheme, displayName, configureOptions);
+        }
+    }
+}
---
+++ CSharp/Library/Microsoft.Bot.Connector.AspNetCore2/BotAuthenticationOptions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace Microsoft.Bot.Connector
+{
+    public sealed class BotAuthenticationOptions : JwtBearerOptions
+    {
+
+        /// <summary>
+        /// The <see cref="ICredentialProvider"/> used for authentication.
+        /// </summary>
+        public ICredentialProvider CredentialProvider { set; get; }
+
+        /// <summary>
+        /// The OpenId configuation.
+        /// </summary>
+        public string OpenIdConfiguration { set; get; } = JwtConfig.ToBotFromChannelOpenIdMetadataUrl;
+
+        /// <summary>
+        /// Flag indicating if emulator tokens should be disabled.
+        /// </summary>
+        public bool DisableEmulatorTokens { set; get; } = false;
+    }
+}
