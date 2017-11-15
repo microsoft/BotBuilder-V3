36028922305d7f6405fdd03766f7d1033b1bd4da
Tom Laird-McConnell <tomlm@microsoft.com>
2017-11-14 20:26:21 -0800

    downgrade to 4.6

--- CSharp/Library/Microsoft.Bot.Connector.Standard/AssemblyInfo.cs
+++ CSharp/Library/Microsoft.Bot.Connector.Standard/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("3.12.2.1")]
-[assembly: AssemblyFileVersion("3.12.2.1")]
+[assembly: AssemblyVersion("3.12.2.2")]
+[assembly: AssemblyFileVersion("3.12.2.2")]
 
 //[assembly: AssemblyKeyFileAttribute(@"..\\..\\buildtools\\35MSSharedLib1024.snk")] 
 //[assembly: AssemblyDelaySignAttribute(true)]
