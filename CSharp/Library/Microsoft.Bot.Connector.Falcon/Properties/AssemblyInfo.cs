6bb74d927633c565e35f7772a9323b280e87a175
Tom Laird-McConnell <tomlm@microsoft.com>
2017-11-27 09:45:20 -0800

    bump everything to 3.12.2.4

--- CSharp/Library/Microsoft.Bot.Connector.Falcon/Properties/AssemblyInfo.cs
+++ CSharp/Library/Microsoft.Bot.Connector.Falcon/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.12.2.2")]
-[assembly: AssemblyFileVersion("3.12.2.2")]
+[assembly: AssemblyVersion("3.12.2.4")]
+[assembly: AssemblyFileVersion("3.12.2.4")]
 
 //[assembly: AssemblyKeyFileAttribute(@"..\\..\\buildtools\\35MSSharedLib1024.snk")] 
 //[assembly: AssemblyDelaySignAttribute(true)]
--- CSharp/Library/Microsoft.Bot.Connector.NetCore/Properties/AssemblyInfo.cs
+++ CSharp/Library/Microsoft.Bot.Connector.NetCore/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Resources;
 
-[assembly: AssemblyVersion("3.12.2.3")]
-[assembly: AssemblyFileVersion("3.12.2.3")]
+[assembly: AssemblyVersion("3.12.2.4")]
+[assembly: AssemblyFileVersion("3.12.2.4")]
 
 //[assembly: AssemblyKeyFileAttribute(@"..\\..\\buildtools\\35MSSharedLib1024.snk")] 
 //[assembly: AssemblyDelaySignAttribute(true)]
