2136ed466fea01c1d4e98b15912f4a77aa6f8a1f
Francisco Ponce Gomez <v-frgome@microsoft.com>
2018-04-20 12:34:29 -0700

    Changes after review

--- CSharp/Generator/BotApplication/Global.asax.cs
+++ CSharp/Generator/BotApplication/Global.asax.cs
@@ -16,17 +16,22 @@ namespace $safeprojectname$
             Conversation.UpdateContainer(
             builder =>
             {
+                builder.RegisterModule(new AzureModule(Assembly.GetExecutingAssembly()));
+
+                // Bot Storage: Here we register the state storage for your bot. 
+                // Default store: volatile in-memory store - Only for prototyping!
+                // We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+                // For samples and documentation, see: [https://github.com/Microsoft/BotBuilder-Azure](https://github.com/Microsoft/BotBuilder-Azure)
                 var store = new InMemoryDataStore();
+
+                // Other storage options
+                // var store = new TableBotDataStore("...DataStorageConnectionString..."); // requires Microsoft.BotBuilder.Azure Nuget package 
+                // var store = new DocumentDbBotDataStore("cosmos db uri", "cosmos db key"); // requires Microsoft.BotBuilder.Azure Nuget package 
+
                 builder.Register(c => store)
-                          .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
-                          .AsSelf()
-                          .SingleInstance();
-                builder.Register(c => new CachingBotDataStore(store,
-                          CachingBotDataStoreConsistencyPolicy
-                          .ETagBasedConsistency))
-                          .As<IBotDataStore<BotData>>()
-                          .AsSelf()
-                          .InstancePerLifetimeScope();
+                    .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                    .AsSelf()
+                    .SingleInstance();
 
             });
         }
