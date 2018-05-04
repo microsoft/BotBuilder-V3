187c32a63a9d9f3811394afdea30d5836f2a6cfb
Tom Laird-McConnell <tomlm@microsoft.com>
2018-05-04 14:59:24 -0700

    bump version (#4562)

--- CSharp/Library/Microsoft.Bot.Connector.NetCore/Properties/AssemblyInfo.cs
+++ CSharp/Library/Microsoft.Bot.Connector.NetCore/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Resources;
 
-[assembly: AssemblyVersion("3.15.0.0")]
-[assembly: AssemblyFileVersion("3.15.0.0")]
+[assembly: AssemblyVersion("3.15.1.0")]
+[assembly: AssemblyFileVersion("3.15.1.0")]
 
 //[assembly: AssemblyKeyFileAttribute(@"..\\..\\buildtools\\35MSSharedLib1024.snk")] 
 //[assembly: AssemblyDelaySignAttribute(true)]
