01696b9c6569eab747a61f43c4e7c2e954671265
Tom Laird-McConnell <tomlm@microsoft.com>
2018-05-06 14:34:23 -0700

    update echobot to use dialog for response.

--- CSharp/EchoBot/Controllers/MessagesController.cs
+++ CSharp/EchoBot/Controllers/MessagesController.cs
@@ -1,14 +1,36 @@
-﻿using Microsoft.Bot.Connector;
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace EchoBot.Controllers
 {
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            // calculate something for us to return
+            int length = (activity.Text ?? string.Empty).Length;
+
+            // return our reply to the user
+            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+
     [BotAuthentication]
     public class MessagesController : ApiController
     {
@@ -17,10 +39,11 @@ namespace EchoBot.Controllers
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                MicrosoftAppCredentials.AutoTokenRefreshTimeSpan = TimeSpan.FromSeconds(30);
+                await Conversation.SendAsync(activity, () => new RootDialog());
+                //MicrosoftAppCredentials.AutoTokenRefreshTimeSpan = TimeSpan.FromSeconds(30);
 
-                ConnectorClient client = new ConnectorClient(new Uri(activity.ServiceUrl), new MicrosoftAppCredentials());
-                await client.Conversations.ReplyToActivityAsync(activity.CreateReply(activity.Text));
+                //ConnectorClient client = new ConnectorClient(new Uri(activity.ServiceUrl), new MicrosoftAppCredentials());
+                //await client.Conversations.ReplyToActivityAsync(activity.CreateReply(activity.Text));
             }
         }
     }
