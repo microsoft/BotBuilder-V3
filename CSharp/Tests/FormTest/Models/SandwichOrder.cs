fdc73d807b6929e29c4f2c69500737410756bbd1
Carlos Castro <carlosscastro@users.noreply.github.com>
2018-06-14 15:34:50 -0700

    Formflow: Bring tests back to life. Still 2 failing but all others passing and form test program working end to end. (#4742)

---
+++ CSharp/Tests/FormTest/Models/SandwichOrder.cs
@@ -0,0 +1,77 @@
+﻿// 
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// 
+// Microsoft Bot Framework: http://botframework.com
+// 
+// Bot Builder SDK GitHub:
+// https://github.com/Microsoft/BotBuilder
+// 
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+// 
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+
+namespace FormTest.Models.SimpleSandwich
+{
+    public enum SandwichOptions
+    {
+        BLT, BlackForestHam, BuffaloChicken, ChickenAndBaconRanchMelt, ColdCutCombo, MeatballMarinara,
+        OvenRoastedChicken, RoastBeef, RotisserieStyleChicken, SpicyItalian, SteakAndCheese, SweetOnionTeriyaki, Tuna,
+        TurkeyBreast, Veggie
+    };
+    public enum LengthOptions { SixInch, FootLong };
+    public enum BreadOptions { NineGrainWheat, NineGrainHoneyOat, Italian, ItalianHerbsAndCheese, Flatbread };
+    public enum CheeseOptions { American, MontereyCheddar, Pepperjack };
+    public enum ToppingOptions
+    {
+        Avocado, BananaPeppers, Cucumbers, GreenBellPeppers, Jalapenos,
+        Lettuce, Olives, Pickles, RedOnion, Spinach, Tomatoes
+    };
+    public enum SauceOptions
+    {
+        ChipotleSouthwest, HoneyMustard, LightMayonnaise, RegularMayonnaise,
+        Mustard, Oil, Pepper, Ranch, SweetOnion, Vinegar
+    };
+
+    [Serializable]
+    public class SandwichOrder
+    {
+        public SandwichOptions? Sandwich;
+        public LengthOptions? Length;
+        public BreadOptions? Bread;
+        public CheeseOptions? Cheese;
+        public List<ToppingOptions> Toppings;
+        public List<SauceOptions> Sauce;
+
+        public static IForm<SandwichOrder> BuildForm()
+        {
+            return new FormBuilder<SandwichOrder>()
+                    .Message("Welcome to the simple sandwich order bot!")
+                    .Build();
+        }
+    };
+}
