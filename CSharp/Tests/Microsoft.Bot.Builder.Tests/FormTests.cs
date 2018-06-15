13f05fbe9ed8ae18e6745cee6f57a0f689809d87
Chris McConnell <chrimc@microsoft.com>
2018-06-14 22:49:26 -0700

    Fix bug in non-serialization code where a closure marked as do not serialize would be serialized because it was thought to be autofac. Fix FormFlow test with incorrect oracle. Update FormFlow tests to not rely on samples. Update localization files to latest MULE output. Fix bug where multiple language resources conflict.

--- CSharp/Tests/Microsoft.Bot.Builder.Tests/FormTests.cs
+++ CSharp/Tests/Microsoft.Bot.Builder.Tests/FormTests.cs
@@ -51,8 +51,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
-using FormTest.Models.SimpleSandwich;
-using FormTest.Models.AnnotatedSandwich;
 
 namespace Microsoft.Bot.Builder.Tests
 {
