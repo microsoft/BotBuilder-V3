702d92b086e0f2206833088fc9e62a3a92d64f0a
Jonathan Spruance (Insight Global) <v-jonspr@microsoft.com>
2020-02-25 15:06:58 -0800

    Eric's validate claims changes

--- Node/core/skills-validator/lib/jwtTokenValidation.js
+++ Node/core/skills-validator/lib/jwtTokenValidation.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -46,12 +47,14 @@ var JwtTokenValidation;
 (function (JwtTokenValidation) {
     function authenticateRequest(activity, authHeader, credentials, serviceUrl, authConfig) {
         return __awaiter(this, void 0, void 0, function () {
-            var isAuthDisabled, claimsIdentity;
+            var xyz, isAuthDisabled, claimsIdentity;
             return __generator(this, function (_a) {
                 switch (_a.label) {
                     case 0:
                         if (!authConfig) {
                             authConfig = new authenticationConfiguration_1.AuthenticationConfiguration();
+                            xyz = 'test';
+                            console.log(xyz);
                         }
                         if (!!authHeader.trim()) return [3, 2];
                         return [4, credentials.isAuthenticationDisabled()];
